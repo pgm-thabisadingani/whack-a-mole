c71d45d4123a11268dc42efd7dbe6f38b82ffb55
pgm-thabisadingani <71275598+pgm-thabisadingani@users.noreply.github.com>
2022-12-06 20:28:40 +0100

    added mole

--- src/components/mole/Mole.tsx
+++ src/components/mole/Mole.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import mole from '../../assets/WAM_Mole.png';
+import hole from '../../assets/WAM_Hole.png';
 
 /**
  * Interface
@@ -13,27 +15,48 @@ const Mole = ({ id }: Props) => {
   const [randomNr, setRandomNr] = useState(0);
   const [counter, setCounter] = useState(0);
 
-  // random nr generator function to generate
-
+  // random nr generator function
   const randomNumberInRange = (min: number, max: number) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   };
 
+  // set interval between
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // generate random number between 1 and 12
+      setRandomNr(randomNumberInRange(1, 12));
+    }, 2000); // runs every 1 second
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   // callback function
 
   const handleClick = () => {
-    setRandomNr(randomNumberInRange(1, 12));
     if (randomNr === id) {
       setCounter(counter + 1);
     } else {
-      setCounter(counter + 0);
+      setCounter(counter);
     }
   };
 
-  console.log(counter);
+  const showMole = () => {
+    return (
+      <>
+        {randomNr === id ? (
+          <img src={mole} alt="mole" />
+        ) : (
+          <img src={hole} alt="hole" />
+        )}
+      </>
+    );
+  };
+
+  console.log(id + ' : ' + randomNr + ' : ' + counter);
   // function if random number === id, increase count by one and hide yes
 
-  return <div onClick={handleClick}>hello</div>;
+  return <div onClick={handleClick}>{showMole()}</div>;
 };
 
 export default Mole;
